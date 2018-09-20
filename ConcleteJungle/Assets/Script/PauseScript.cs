3a7a4058ac76a046d21ea2803b4d9455b837e238
小塚捷人 <>
2018-09-20 15:40:05 +0900

    脳死プレイ

---
+++ ConcleteJungle/Assets/Script/PauseScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour
+{
+    public static int ito_flag = 1;
+    //　ポーズした時に表示するUI
+    [SerializeField]
+    public GameObject pauseUI;
+    public GameObject pauseButton;
+
+    public void PauseButtonDown()
+    {
+        //　ポーズUIのアクティブ、非アクティブを切り替え
+        pauseUI.SetActive(!pauseUI.activeSelf);
+        pauseButton.SetActive(!pauseButton.activeSelf);
+
+        //　ポーズUIが表示されてる時は停止
+        if (pauseUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+            //　ポーズUIが表示されてなければ通常通り進行
+            ito_flag = 0;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            ito_flag = 1;
+        }
+    }
+
+    public static int GetItoFlag()
+    {
+        return ito_flag;
+    }
+}
---
+++ ConcleteJungle/Assets/Script/ResultDirector.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultDirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AgainButtonDown()
+    {
+		Debug.Log("Solo");
+        SceneManager.LoadScene("Solo");
+    }
+
+    public void AgainButton2Down()
+    {
+		Debug.Log("Multi");
+        //SceneManager.LoadScene("Multi");
+    }
+
+    public void SelectButtonDown()
+    {
+		Debug.Log("Select");
+        SceneManager.LoadScene("Select");
+    }
+
+    public void TitleButtonDown()
+    {
+		Debug.Log("Title");
+        SceneManager.LoadScene("Title");
+    }
+
+    public void EndButtonDown()
+    {
+		Debug.Log("End");
+        Application.Quit();
+    }
+}
