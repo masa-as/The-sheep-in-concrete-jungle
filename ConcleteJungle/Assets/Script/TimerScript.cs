b9e57e19bdd25870a673c0051d04db8f47617f74
masashi iijima <masashiiijima@dcl.cs.waseda.ac.jp>
2018-09-21 10:53:28 +0900

    リトライ時の動かなかったのを動くようにした

--- ConcleteJungle/Assets/Script/TimerScript.cs
+++ ConcleteJungle/Assets/Script/TimerScript.cs
@@ -18,7 +18,7 @@ public class TimerScript : MonoBehaviour {
 		int minute = (int)time/60;//分.timeを60で割った値.
 		int second = (int)time;//秒.timeを60で割った余り.
 		int msecond = (int)(time*100%100);
-		string minText, secText, msecText;//分・秒を用意.
+		string secText, msecText;//分・秒を用意.
 
         if (second < 10)
             secText = "00" + second.ToString();
