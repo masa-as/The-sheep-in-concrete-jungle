86b2018bd9156e001fc17ec5487cd14e542bd227
小塚捷人 <>
2018-09-26 05:45:06 +0900

    簡単なfix

--- ConcleteJungle/Assets/Script/PlayerController_Photon.cs
+++ ConcleteJungle/Assets/Script/PlayerController_Photon.cs
@@ -68,49 +68,54 @@ public class PlayerController_Photon : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        pos = transform.position;
-        if(swing){
-            pos_ito = pos;
-            dirY = ceiling_pos.y - pos.y;
-            dirX = dirY;
-            leng = Mathf.Sqrt(dirX * dirX + dirY * dirY) * 0.095f;//画像差し替えたら調節
-            if ((wool_count - leng * 20f) > 0)
+        if(this.myPhotonView.isMine){
+            pos = transform.position;
+            if (swing)
             {
-                ito = Instantiate(itoPrafab) as GameObject;
-                ito.transform.Rotate(0f, 0f, -45.0f);
-                ito.transform.localScale = new Vector3(0.4f, leng, 0f);
-                pos_ito.x += dirX / 2.0f + 1.4f;//画像差し替えたら調節
-                pos_ito.y += dirY / 2.0f + 1.4f;//画像差し替えたら調節
-                ito.transform.position = pos_ito;
+                pos_ito = pos;
+                dirY = ceiling_pos.y - pos.y;
+                dirX = dirY;
+                leng = Mathf.Sqrt(dirX * dirX + dirY * dirY) * 0.095f;//画像差し替えたら調節
+                if ((wool_count - leng * 20f) > 0)
+                {
+                    ito = Instantiate(itoPrafab) as GameObject;
+                    ito.transform.Rotate(0f, 0f, -45.0f);
+                    ito.transform.localScale = new Vector3(0.4f, leng, 0f);
+                    pos_ito.x += dirX / 2.0f + 1.4f;//画像差し替えたら調節
+                    pos_ito.y += dirY / 2.0f + 1.4f;//画像差し替えたら調節
+                    ito.transform.position = pos_ito;
 
-                joint = gameObject.AddComponent<HingeJoint>();
-                Rigidbody rb = ito.GetComponent<Rigidbody>();
-                joint.connectedBody = rb;
-                joint.anchor = new Vector3(0.2f, 0.0f, 0);
-                joint.axis = new Vector3(0, 0, 1);
+                    joint = gameObject.AddComponent<HingeJoint>();
+                    Rigidbody rb = ito.GetComponent<Rigidbody>();
+                    joint.connectedBody = rb;
+                    joint.anchor = new Vector3(0.2f, 0.0f, 0);
+                    joint.axis = new Vector3(0, 0, 1);
 
-                joint_ito = ito.gameObject.AddComponent<HingeJoint>();
-                Rigidbody rb_ceil = ceiling.GetComponent<Rigidbody>();
-                joint_ito.connectedBody = rb_ceil;
-                joint_ito.anchor = new Vector3(0, dirY / 2f + 0.6f, 0);//画像差し替えたら調節
-                joint_ito.axis = new Vector3(0, 0, 1);
+                    joint_ito = ito.gameObject.AddComponent<HingeJoint>();
+                    Rigidbody rb_ceil = ceiling.GetComponent<Rigidbody>();
+                    joint_ito.connectedBody = rb_ceil;
+                    joint_ito.anchor = new Vector3(0, dirY / 2f + 0.6f, 0);//画像差し替えたら調節
+                    joint_ito.axis = new Vector3(0, 0, 1);
 
-                //糸の長さによる
-                rb_player.AddForce(leng * 10, -leng * 10, 0, ForceMode.Impulse);
-                wool_count -= leng * 7;
-                swing = false;
+                    //糸の長さによる
+                    rb_player.AddForce(leng * 10, -leng * 10, 0, ForceMode.Impulse);
+                    wool_count -= leng * 7;
+                    swing = false;
+                }
             }
+            if (jump)
+            {
+                anim.SetBool("Swinging", false);
+                Destroy(ito);
+                Destroy(joint);
+                joint = null;
+                jump = false;
+            }
+            pos_camera.x = transform.position.x + 20;
+            Camera.main.transform.position = pos_camera;
+            _slider.value = wool_count / 100;
         }
-        if(jump){
-            anim.SetBool("Swinging", false);
-            Destroy(ito);
-            Destroy(joint);
-            joint = null;
-            jump = false;
-        }
-        pos_camera.x = transform.position.x + 20;
-        Camera.main.transform.position = pos_camera;
-        _slider.value = wool_count / 100;
+
     }
 
     private void OnCollisionEnter(Collision collision)
