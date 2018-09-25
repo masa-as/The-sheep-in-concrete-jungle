622a978e89f441942d13bb9b2971fee3099f892b
masashi iijima <masashiiijima@dcl.cs.waseda.ac.jp>
2018-09-25 12:19:04 +0900

    背景アニメーション

---
+++ ConcleteJungle/Assets/Script/BackgroundController2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController2 : MonoBehaviour
+{
+
+    public float speed;
+    //public int spriteCount = 2;
+    public GameObject Hitsuji;
+    public GameObject Background;
+
+
+    void Update()
+    {
+        // 左へ移動
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        if (transform.position.x < Hitsuji.transform.position.x - 58f)
+        {
+            Vector3 tmp = Background.transform.position;
+            tmp.x += 114;
+            Background.transform.position = tmp;
+
+        }
+    }
+    /*
+    void OnBecameInvisible()
+    {
+        // スプライトの幅を取得
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        // 幅ｘ個数分だけ右へ移動
+        transform.position += Vector3.right * width * spriteCount;
+    }
+    */
+}
